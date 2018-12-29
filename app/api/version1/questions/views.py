b0957339f771b8fcc3875ec7757fc56af10be0a5
b0nbon1 <b0nbon@yahoo.com>
2018-12-29 20:08:47 +0300

    [finishes #162882741]endpoint delte and update question

--- app/api/version1/questions/views.py
+++ app/api/version1/questions/views.py
@@ -32,3 +32,28 @@ def ask_question():
 
     ask = Questions().ask_question(author, title, description, tag)    
     return make_response(jsonify(ask)), 200
+
+    @quest.route('/<string:question_id>', methods=['DELETE'])
+def delete_question(question_id):
+
+    # question = [question for question in questions if question['id'] == question_id]
+    if question_id.isdigit():
+        for question in questions:
+            if (question_id) == question["id"]:
+                questions.remove(question)
+                return make_response(jsonify(questions)), 200
+        return make_response(jsonify({"Message": "no question"})), 404
+    return make_response(jsonify({"Message": "Invalid question id"})), 404
+
+
+@quest.route('/<string:question_id>', methods=['PUT'])
+def update_question(question_id):
+    question = [question for question in questions if question['id'] == question_id]
+    if len(question) == 0:
+        abort(404)
+
+    question[0]["title"] = request.json.get("title", question[0]["title"])
+    question[0]["author"] = request.json.get("author", question[0]["title"])
+    question[0]["description"] = request.json.get("description", question[0]["description"])
+    question[0]["tag"] = request.json.get("tag", question[0]["tag"])
+    return jsonify(question)
